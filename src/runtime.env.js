296bda9d10cd93175e4406d26f387f96c5d85835
christianarredondo <christianarredondo@utexas.edu>
2019-12-21 05:52:45 -0600

    add docs for runtime.env

--- src/runtime.env.js
+++ src/runtime.env.js
@@ -1,3 +1,8 @@
 import runtimeEnv from '@mars/heroku-js-runtime-env';
 
+/**
+ * Follows `create-creact-app` build-pack library
+ * to set runtime environment variables
+ * @see https://github.com/mars/create-react-app-buildpack#runtime-configuration
+ */
 export const env = runtimeEnv();
