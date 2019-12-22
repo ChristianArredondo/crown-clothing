da8d56bda646ee1849c410cc29a9ec28a4e2fdfd
christianarredondo <christianarredondo@utexas.edu>
2019-12-22 12:30:10 -0600

    move react front-end to client dir and create basic node server

---
+++ server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const isProd = process.env.NODE_ENV === 'production';
+if (isProd) {
+  require('dotenv').config();
+}
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+if (isProd) {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, error => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Server listening on post ${port}`);
+});
+
+app.get('/ping', (req, res) => {
+  res.send({ yo: 'server is alive' });
+});
