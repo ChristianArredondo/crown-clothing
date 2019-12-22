dbd07f249153928acdd36f7ce1db1be1dcde9c3d
christianarredondo <christianarredondo@utexas.edu>
2019-12-22 10:14:36 -0600

    convert login + signup components to functional using hooks

--- src/components/login-form/login-form.component.jsx
+++ src/components/login-form/login-form.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './login-form.component.scss';
 import FormInput from '../form-input/form-input.component';
@@ -6,56 +6,49 @@ import CrownButton from '../crown-button/crown-button.component';
 import { signInWithGoogleAction, signInWithEmailAction } from '../../redux/user/user.actions';
 import { connect } from 'react-redux';
 
-class LoginForm extends React.Component {
-  state = {
-    email: '',
-    password: ''
-  };
-
-  onFormSubmit = async evt => {
+const LoginForm = ({ dispatchGoogleSignIn, dispatchEmailSignIn }) => {
+  const [userCreds, setCreds] = useState({ email: '', password: '' });
+  const onFormSubmit = async evt => {
     evt.preventDefault();
-    this.props.dispatchEmailSignIn(this.state);
+    dispatchEmailSignIn(userCreds);
+  };
+  const onFormChange = evt => {
+    setCreds({ ...userCreds, [evt.target.name]: evt.target.value });
   };
 
-  onFormChange = evt => {
-    this.setState({ [evt.target.name]: evt.target.value });
-  };
+  return (
+    <div className="login-form">
+      <h2>I already have an account</h2>
+      <span>Sign in with your email and password</span>
 
-  render() {
-    return (
-      <div className="login-form">
-        <h2>I already have an account</h2>
-        <span>Sign in with your email and password</span>
-
-        <form onSubmit={this.onFormSubmit}>
-          <FormInput
-            required
-            type="email"
-            name="email"
-            label="email"
-            value={this.state.email}
-            handleChange={this.onFormChange}
-          />
-          <FormInput
-            required
-            type="password"
-            name="password"
-            label="password"
-            value={this.state.password}
-            handleChange={this.onFormChange}
-          />
-          <br />
-          <div className="buttons">
-            <CrownButton type="submit">Log In</CrownButton>
-            <CrownButton type="button" isGoogle onClick={this.props.dispatchGoogleSignIn}>
-              Login with Google
-            </CrownButton>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+      <form onSubmit={onFormSubmit}>
+        <FormInput
+          required
+          type="email"
+          name="email"
+          label="email"
+          value={userCreds.email}
+          handleChange={onFormChange}
+        />
+        <FormInput
+          required
+          type="password"
+          name="password"
+          label="password"
+          value={userCreds.password}
+          handleChange={onFormChange}
+        />
+        <br />
+        <div className="buttons">
+          <CrownButton type="submit">Log In</CrownButton>
+          <CrownButton type="button" isGoogle onClick={dispatchGoogleSignIn}>
+            Login with Google
+          </CrownButton>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   dispatchGoogleSignIn: () => dispatch(signInWithGoogleAction()),
--- src/components/signup-form/signup-form.component.jsx
+++ src/components/signup-form/signup-form.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './signup-form.scss';
 import FormInput from '../form-input/form-input.component';
@@ -6,77 +6,70 @@ import CrownButton from '../crown-button/crown-button.component';
 import { connect } from 'react-redux';
 import { registerUserAction } from '../../redux/user/user.actions';
 
-class SignupForm extends React.Component {
-  state = {
+const SignupForm = ({ dispatchRegisterUser }) => {
+  const [signupForm, setForm] = useState({
     displayName: '',
     email: '',
     password: '',
     passwordConfirm: ''
-  };
-
-  onSubmitForm = async evt => {
+  });
+  const { email, password, passwordConfirm, displayName } = signupForm;
+  const onSubmitForm = async evt => {
     evt.preventDefault();
-    const { email, password, passwordConfirm } = this.state;
-
     if (password !== passwordConfirm) {
       alert("passwords don't match!");
       return;
     }
-
-    this.props.dispatchRegisterUser({ email, password });
+    dispatchRegisterUser({ email, password });
+  };
+  const onFormChange = evt => {
+    setForm({ ...signupForm, [evt.target.name]: evt.target.value });
   };
 
-  onFormChange = evt => {
-    this.setState({ [evt.target.name]: evt.target.value });
-  };
-
-  render() {
-    const { displayName, email, password, passwordConfirm } = this.state;
-    return (
-      <div className="signup-form">
-        <h2>I don't have an account</h2>
-        <span>Register to create a new account</span>
-        <form onSubmit={this.onSubmitForm}>
-          <FormInput
-            type="text"
-            name="displayName"
-            label="display name"
-            value={displayName}
-            handleChange={this.onFormChange}
-            required
-          />
-          <FormInput
-            type="email"
-            name="email"
-            label="email"
-            value={email}
-            handleChange={this.onFormChange}
-            required
-          />
-          <FormInput
-            type="password"
-            name="password"
-            label="password"
-            value={password}
-            handleChange={this.onFormChange}
-            required
-          />
-          <FormInput
-            type="password"
-            name="passwordConfirm"
-            label="confirm password"
-            value={passwordConfirm}
-            handleChange={this.onFormChange}
-            required
-          />
-          <div className="buttons">
-            <CrownButton type="submit">Register</CrownButton>
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="signup-form">
+      <h2>I don't have an account</h2>
+      <span>Register to create a new account</span>
+      <form onSubmit={onSubmitForm}>
+        <FormInput
+          type="text"
+          name="displayName"
+          label="display name"
+          value={displayName}
+          handleChange={onFormChange}
+          required
+        />
+        <FormInput
+          type="email"
+          name="email"
+          label="email"
+          value={email}
+          handleChange={onFormChange}
+          required
+        />
+        <FormInput
+          type="password"
+          name="password"
+          label="password"
+          value={password}
+          handleChange={onFormChange}
+          required
+        />
+        <FormInput
+          type="password"
+          name="passwordConfirm"
+          label="confirm password"
+          value={passwordConfirm}
+          handleChange={onFormChange}
+          required
+        />
+        <div className="buttons">
+          <CrownButton type="submit">Register</CrownButton>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 const mapDispatchToProps = dispatch => ({
   dispatchRegisterUser: emailAndPassword => dispatch(registerUserAction(emailAndPassword))
